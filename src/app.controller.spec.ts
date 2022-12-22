66fa6e66efd14626d45bcc6fb41278db0dc37d2c
Achmad Ricky Budianto <ricky@aksoro.co.id>
2022-12-22 16:10:38 +0700

    Auto deploy test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hilli Wirld!"', () => {
+      expect(appController.getHello()).toBe('Hilli Wirld!');
     });
   });
 });
